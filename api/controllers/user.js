d7be71335d41c2c9f22b9b52dd18f64dd6047063
unknown <davsoudry@gmail.com>
2018-03-23 18:31:57 -0500

    lowercasing working with CBs

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,7 +2,8 @@ const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
-  const { username, passwordHash } = req.body;
+  let { username, password } = req.body;
+  const passwordHash = password;
   username = username.toLowerCase();
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -15,7 +15,7 @@ const UserSchema = Schema({
   },
   passwordHash: {
     type: String,
-    required: true
+    // required: true
   }
   
 });
@@ -25,20 +25,20 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
-  bcrypt.hash(this.passwordHash, SALT_ROUNDS)
-    .then(hash => {
-      user.password = hash;
-      next();
-    })
-    .catch(err => {
-      next(err);
-    });
+  // bcrypt.hash(this.passwordHash, SALT_ROUNDS)
+  //   .then(hash => {
+  //     user.passwordHash = hash;
+  //     next();
+  //   })
+  //   .catch(err => {
+  //     next(err);
+  //   });
 
-  // bcrypt.hash(this.passwordHash, SALT_ROUNDS, (err, hash) => {
-  //   if (err) return next(err);
-  //   this.passwordHash = hash;
-  //   next();
-  // });
+  bcrypt.hash(this.passwordHash, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+    this.passwordHash = hash;
+    next();
+  });
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
