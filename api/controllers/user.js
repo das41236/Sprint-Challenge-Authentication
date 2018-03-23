5f3e4d30c6dbcb546dc8c751ead96d574b959d4c
unknown <davsoudry@gmail.com>
2018-03-23 12:56:12 -0500

    userModels.js complete

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,15 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const newUser = new User({ username, password });
+  newUser.save((err, savedUser) => {
+    if (err) {
+      res.status(422);
+      res.json({ 'Need both username/PW fields': err.message });
+      return;
+    }
+    res.json(savedUser);
+  });
 };
 
 module.exports = {
